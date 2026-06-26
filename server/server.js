1fae068d9a1f9f244509235a321acbb8027daebb
Premcoder123 <premsairpremsai@gmail.com>
2026-06-26 23:22:06 +0530

    Fix Render port binding

--- server/server.js
+++ server/server.js
@@ -12,7 +12,6 @@ const thumbnailRoutes = require("./routes/thumbnailRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const HOST = process.env.HOST || "127.0.0.1";
 
 connectDB();
 
@@ -42,6 +41,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(PORT, HOST, () => {
-  console.log(`Server running on http://${HOST}:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
